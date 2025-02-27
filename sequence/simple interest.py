88e62e0e9e443abaa43417aee0153f07e01a6ae2
nandinishende <adityashende443@gmail.com>
2025-02-27 13:19:38 +0000

    simple interest

---
+++ sequence/simple interest.py
@@ -0,0 +1,5 @@
+num1=int(input("enter principal number:"))
+num2=int(input("enter rate of interest:"))
+num3=int(input("enter time in year:"))
+simple_interest=(num1*num2*num3)/100
+print("simple interest is:",simple_interest)
