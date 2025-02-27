5e4489541602e5744958658b891f816a8b71c98d
nandinishende <adityashende443@gmail.com>
2025-02-27 08:12:52 +0000

    modulus and floor division operator

---
+++ sequence/modulus and floor.py
@@ -0,0 +1,6 @@
+num1=int(input("enter first number :"))
+num2=int(input("enter second number :"))
+mod=num1%num2
+floor=num1//num2
+print("modulus of two numbers=",mod)    
+print("floor division of two numbers=",floor)
