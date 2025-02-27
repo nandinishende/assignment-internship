dcd1fa8fff62f602a420f7760d87e73637b8ab48
nandinishende <adityashende443@gmail.com>
2025-02-27 13:13:35 +0000

    perimeter of rectangle

---
+++ sequence/perimeter of the rectangle.py
@@ -0,0 +1,4 @@
+length=int(input("enter length of the rectangle:"))
+breadth=int(input("enter breadth of the rectangle:"))
+perimeter=2*(length+breadth)  
+print("perimeter of the rectangle is:",perimeter)
